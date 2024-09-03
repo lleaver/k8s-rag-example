7e0349975a84f7d339dc56b52a28edc825c5f587
Linnea Leaver <lleaver@nvidia.com>
2024-09-03 13:30:48 -0400

    update chain

--- chain.py
+++ chain.py
@@ -3,18 +3,12 @@ Simple pipeline for testing RAG pipeline with NIM microservices on K8s
 Uses LLM and Embedder NIMs, NVIDIA langchain connectors and FAISS vector store
 """
 
-import getpass
-
 from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
-
-#get api key
-password = getpass.getpass("Enter your NVAPI Personal Key: ")
-nvapi_key = password
 
 ## Load data
 data_file = "path to .txt file here"
@@ -26,13 +20,13 @@ docs = loader.load_and_split(text_splitter=splitter)
 ## Define pipeline components: LLM, Embedder
 # add base_url to these to connect to local
 llm = ChatNVIDIA(
-    model="meta/llama2-70b", 
-    nvidia_api_key=nvapi_key
+    model="meta/llama3-8b-instruct",
+    base_url="http://<worker-node>:31001",
 )
 
 embedder = NVIDIAEmbeddings(
   model="nvidia/nv-embedqa-e5-v5", 
-  api_key=nvapi_key, 
+  base_url="http://<worker-node>:31002",
   truncate="NONE", 
 )
 
